1e1d6d81f8c590a37c1453f5ae252f2d471f41e5
TalipAlkoc <alkoctalip@gmail.com>
2025-05-14 19:51:02 +0300

    pause system added

--- Assets/_GameAssets/Scripts/UI/TimerUI.cs
+++ Assets/_GameAssets/Scripts/UI/TimerUI.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -15,28 +16,72 @@ public class TimerUI : MonoBehaviour
 
     private float _elapsedTime;//geçen süre
 
+    private bool _isTimeRunning;
+    private Tween _rotationTween;
+
+
     private void Start()
     {
         PlayRotationAnimation();
         StartTimer();
+
+        GameManager.Instance.OnGameStateChanged += GameManager_OnGameStateChanged;
+    }
+
+    private void GameManager_OnGameStateChanged(GameState gameState)
+    {
+        switch (gameState)
+        {
+            case GameState.Pause:
+                //PAUSE TIMER
+                PauseTımer();
+                break;
+            case GameState.Resume:
+                ResumeTımer();
+                //RESUME TIMER
+                break;
+               
+        }
     }
 
     private void PlayRotationAnimation()
     {
-        _timerRotatableTransform.DORotate(new Vector3(0f, 0f, -360f), _rotationDuration, RotateMode.FastBeyond360)
+       _rotationTween = _timerRotatableTransform.DORotate(new Vector3(0f, 0f, -360f), _rotationDuration, RotateMode.FastBeyond360)
             .SetLoops(-1, LoopType.Restart)
             .SetEase(_rotationEase);
     }
 
     private void StartTimer()
     {
+        _isTimeRunning = true;
         _elapsedTime = 0f;
         InvokeRepeating(nameof(UpadateTimerUI), 0f, 1f);
+    }
+
+    private void PauseTımer()
+    {
+        _isTimeRunning = false;
+        CancelInvoke(nameof(UpadateTimerUI));
+        _rotationTween.Pause();
+    }
+
+    private void ResumeTımer()
+    {
+        if (!_isTimeRunning)
+        {
+            _isTimeRunning = true;
+            InvokeRepeating(nameof(UpadateTimerUI), 0f, 1f);
+            _rotationTween.Play();
+        }
     }
 
 
     private void UpadateTimerUI()
     {
+        if (!_isTimeRunning)
+        {
+            return;
+        }
         _elapsedTime += 1f;
 
         int minutes = Mathf.FloorToInt(_elapsedTime / 60f);
