f4e31a7cf3904496f8d5e72fa148db97d85dbcf9
TalipAlkoc <alkoctalip@gmail.com>
2025-06-01 17:07:31 +0300

    ai cat added

--- Assets/_GameAssets/Scripts/Gameplay/Helpers/Consts.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Helpers/Consts.cs
@@ -7,6 +7,13 @@ public class Consts
         public const string GAME_SCENE = "GameScene1";
     }
 
+    public struct Layers
+    {
+        public const string GROUND_LAYER = "Ground";
+        public const string FLOOR_LAYER = "Floor";
+
+    }
+
 
     public struct PlayerAnimations
     {
